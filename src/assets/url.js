99f7c27091820ff9836b893d2af6214fe252cab5
Youssef Eraky <yeraky07@gmail.com>
2025-09-12 15:26:55 +0200

    update url

--- src/assets/url.js
+++ src/assets/url.js
@@ -1,8 +1,2 @@
 // export const BASE_URL = "http://localhost:5000"
 export const BASE_URL = "https://full-ecommerce-backend.vercel.app/api"
-
-// const BASE_URL = import.meta.env.PROD 
-//   ? 'https://ecommerce-backend-pknk.vercel.app'  // حط رابط الـ backend بتاعك هنا
-//   : 'http://localhost:5000';
-
-// export default BASE_URL;
